3a4a7445ad8a119c5db0990db31c6217b5a43fdc
Mykyta Shyshov <workmailnikita@gmail.com>
2026-03-07 00:05:41 +0200

    Make HTTP error code a strict union type

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export { default as AxiosUtils } from './utils/AxiosUtils';
 export { botServiceAuthGuard, parseBearerToken } from './auth/bot-service-auth';
 export { getPrismaClient, prisma } from './db/prisma';
-export { createErrorPayload, sendError } from './http/error';
+export { createErrorPayload, sendError, ERROR_CODES } from './http/error';
 export { withBotServiceAuth } from './http/with-bot-service-auth';
 export { getUserLocalToday } from './utils/user-local-today';
 
 export type { BotServiceAuthResult } from './auth/bot-service-auth';
-export type { ErrorPayload } from './http/error';
+export type { ErrorCode, ErrorPayload } from './http/error';
 export type { ApiHandler } from './http/with-bot-service-auth';
